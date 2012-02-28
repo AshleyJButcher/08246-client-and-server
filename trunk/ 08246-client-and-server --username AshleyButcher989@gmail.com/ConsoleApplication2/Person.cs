e00f810d5268c533c34aebcf947f78b0ed13c7df
AshleyButcher989@googlemail.com <AshleyButcher989@googlemail.com@d7993591-a564-0f9c-27c1-c2aea3f40c8b>
2012-02-28 23:48:25 +0000

    i will commit these bitch'in changes

---
+++ trunk/ 08246-client-and-server --username AshleyButcher989@gmail.com/ConsoleApplication2/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhereIsServer
+{
+    class Person
+    {
+        private string name;
+        private string location;
+
+        public Person(string name, string location)
+        {
+            SetLocation(location);
+            SetName(name);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string inputstring)
+        {
+            name = inputstring;
+        }
+
+        public string GetLocation()
+        {
+            return location;
+        }
+
+        public void SetLocation(string inputstring)
+        {
+            location = inputstring;
+        }
+    }
+}
